d9cf4364b8d78feaf9b5b59b37fd2d2d763ea7a2
Benjamin Hirsch <mail@benjaminhirsch.net>
2024-02-26 22:48:10 +0100

    Simplify OrderedListTest and remove unused import

--- test/Elements/OrderedListTest.php
+++ test/Elements/OrderedListTest.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace BenjaminHirsch\Html\Test\Elements;
 
-use BenjaminHirsch\Html\Attribute\Id;
-use BenjaminHirsch\Html\Element\OrderedList;
 use PHPUnit\Framework\TestCase;
 
-use function BenjaminHirsch\Html\a;
-use function BenjaminHirsch\Html\href;
 use function BenjaminHirsch\Html\li;
 use function BenjaminHirsch\Html\ol;
-use function BenjaminHirsch\Html\target;
 
 final class OrderedListTest extends TestCase
 {
@@ -24,30 +19,4 @@ final class OrderedListTest extends TestCase
         // Test list with 3 empty children
         self::assertEquals('<ol><li></li><li></li><li></li></ol>', ol(li(), li(), li())->render());
     }
-
-    public function testDefault(): void
-    {
-        $ol = ol(
-            li('Hello Word'),
-            li(li('Nested List')),
-            li(
-                new Id('id1'),
-                a(
-                    new Id('id2'),
-                    href('/foo'),
-                    target('_blank'),
-                    'Just a link',
-                ),
-            )->if(false),
-        );
-
-        self::assertInstanceOf(OrderedList::class, $ol);
-
-        echo $ol->render();
-    }
-
-//    public function testUl()
-//    {
-//        echo ul()->render();
-//    }
 }
